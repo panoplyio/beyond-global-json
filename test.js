61a56fc2bc2de2fd1e2b823911486ca27b130c01
alonweissfeld <alon.weissfeld@panoply.io>
2016-08-03 18:09:25 +0300

    Parsing test

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+require( 'sugar' );
 var assert = require( 'assert' );
 var Promise = require( 'bluebird' );
 var parseBig = require( './index' );
@@ -6,7 +7,7 @@ var MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER;
 var MIN_SAFE_INTEGER = Number.MIN_SAFE_INTEGER;
 
 describe( 'Parse Big', function () {
-    it( 'response with bigint library error', function ( done ) {
+    it( 'error', function ( done ) {
         var badJsonString = '{"name":"somename}';
 
         Promise.resolve( badJsonString )
@@ -19,7 +20,41 @@ describe( 'Parse Big', function () {
                 done();
             })
     })
-    
+
+    it( 'parse', function ( done ) {
+        var addedDigit = '1';
+        var values = [ MAX_SAFE_INTEGER, MIN_SAFE_INTEGER ];
+
+        // add another digit for each tested value
+        values = values.map( function ( val ) {
+            return quotes( val ).concat( addedDigit );
+        })
+
+        var results = [];
+        Promise.resolve( values )
+            .each( function ( val ) {
+                var parsed = parse( val );
+                results.push( parsed );
+            })
+            .return( results )
+            .each( function ( result ) {
+                result = new String( result );
+                var lastDigit = result[ result.length - 1 ];
+
+                // we want to test that while the module is
+                // required, it wouldn't use JSON.parse to
+                // round our values when they're bigger than
+                // Number.MAX_SAFE_INTEGER or smaller
+                // than Number.MIN_SAFE_INTEGER
+                assert.equal( lastDigit, addedDigit )
+            })
+            .then( function () {
+                done();
+            })
+            .catch( function ( err ) {
+                done( err );
+            })
+    })
 });
 
 /**
@@ -30,4 +65,14 @@ describe( 'Parse Big', function () {
  */
 function parse ( input ) {
     return JSON.parse( input );
+}
+
+/**
+ * Quote a given input
+ *
+ * @param {Mixed} input - given input
+ * @return {String} returns the quoted input
+ */
+function quotes ( input ) {
+    return '{1}'.assign( input );
 }
