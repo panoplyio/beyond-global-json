ce82b10de8275b23c2b4440aafc62656a4cdf308
alonweissfeld <alon.weissfeld@panoply.io>
2016-08-07 19:37:43 +0300

    Better grammer: overwrites -> overrides

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Javascript uses floating numbers to store all numbers. That means that past a certain point (Number.MAX_SAFE_INTEGER), integers will be incorrect due to floating point precision.
 
-beyond-global-json module provides a simple overwrite of the following global JSON object methods, to handle the described above:
+beyond-global-json module provides a simple override of the following global JSON object methods, to handle the described above:
 * `parse`
 * `stringify`
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-This module overwrites the 'parse' and 'stringify' methods for
+This module overrides the 'parse' and 'stringify' methods for
 the global JSON object, to handle parsing of large numbers.
 This is due to the fact the Javascript uses floating numbers to
 store all numbers. Therefore, past a certain point (Number.MAX_SAFE_INTEGER),
@@ -7,12 +7,12 @@ integers will be incorrect due to the floating point percision.
  */
 var JSONbigInt = require( 'json-bigint' );
 
-module.exports = overwrite();
+module.exports = override();
 
 /**
- * Overwrites the global JSON methods
+ * Overrides the global JSON methods
  */
-function overwrite () {
+function override () {
     JSON.parse = JSONbigInt.parse;
     JSON.stringify = JSONbigInt.stringify;
 }
